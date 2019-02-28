3911b39968e5ef5c9ec1bc10064b7e9b7dad8514
nokia6680 <artur.ballod@gmail.com>
2019-02-28 20:02:52 +0200

    1366-768

--- js/script.js
+++ js/script.js
@@ -151,9 +151,9 @@ $(document).ready(function() {
   $('.gallery-carousel').slick({
     infinite: true,
     slidesToShow: 6,
-    slidesToScroll: 1,
+    slidesToScroll: 3,
     dots: false,
-    arrows: true,
+    arrows: false,
     prevArrow: '<div class="prev-2"></div>',
     nextArrow: '<div class="next-2"></div>',
     responsive: [{
